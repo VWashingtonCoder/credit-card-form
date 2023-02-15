1234e6f197a399711a75bbdf39b895ae43b0936c
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-02-15 11:14:21 -0700

    Set up input base states in form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,17 +2,51 @@ import React from "react";
 import InputBase from "../InputBase/InputBase";
 import './Form.css';
 
+const INIT_CARD = {
+    card: '',
+    cardHolder: '',
+    expiry: '',
+    securityCode: ''
+}
+
 class Form extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cardData: INIT_CARD,
+            maxLength: 19
+        }
+    }
+
+    handleInputData = (e) => {
+        this.setState((prevState) => ({  
+            cardData: { ...prevState.cardData, [e.target.name]: e.target.value }
+        }))
+    }
 
     render() {
+        const inputData = [
+            { label: 'Card Number', name: 'card', type: 'text'},
+            { label: 'CardHolder\'s Name', name: 'cardHolder', type: 'text'},
+            { label: 'Expiry Date (MM/YY)', name: 'expiry', type: 'text'},
+            { label: 'Security Code', name: 'securityCode', type: 'text'},
+        ]
+
         return (
             <div>
                 <h1>Add New Card</h1>
                 <form>
-                    <InputBase />
-                    <InputBase />
-                    <InputBase />
-                    <InputBase />
+                    {inputData.length ? inputData.map((item) => (
+                        <InputBase 
+                            placeholder={item.label}
+                            type={item.type}
+                            value={this.state.cardData && this.state.cardData[item.name]}
+                            onChange={this.handleInputData}
+                            autoComplete="off"
+                            maxLength={this.state.maxLength}
+                            name={item.name}
+                        />
+                    )): null}
                     <div className="btn-wrapper">
                         <InputBase type="submit" value="Add Card"/>
                     </div>
